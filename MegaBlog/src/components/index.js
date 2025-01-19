11b20fabb97dc38d0a59f61b8268118627c85485
Skcshivam <shichoudhary999@gmail.com>
2025-01-19 22:12:57 +0530

    created component and signup and login

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,5 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from './container/Container'
 import Logo from './Logo'
 import LogoutBtn from "./Header/Logoutbtn";
+import Button from './Button'
+import Input from "./input";
 
 export { Header, Footer, Container,LogoutBtn,Logo };
